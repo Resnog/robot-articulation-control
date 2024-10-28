d5b7f00ca1399ed4adb2e37bd3700fb5b1dbfb3d
Resnog <sergio.m.gonzalez.a.94@gmail.com>
2024-10-28 18:03:19 +0200

    Add some macros for the test.c file

--- src/test.c
+++ src/test.c
@@ -2,14 +2,17 @@
 #include "stepper_control.h"
 #include "encoder.h"
 
+#define SAMPLE_RATE_1KHZ (1000)
+#define PULSES_PER_REV (600)
+#define REF_POSITION_IN_RAD (0)
 int main(){
 
     // Pre-loop sequence
 
     // Configuration
-    encoder_t enc = encoder_init(1000,
-                                 600,
-                                 0);
+    encoder_t enc = encoder_init(SAMPLE_RATE_1KHZ,
+                                 PULSES_PER_REV,
+                                 REF_POSITION_IN_RAD);
 
     stepper_t control = stepper_init();
 
