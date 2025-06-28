43cfdac528a2c38dd57de7f31737c2ac8ef13c26
Resnog <sergio.m.gonzalez.a.94@gmail.com>
2025-06-28 23:45:34 +0200

    Add KNode protocol logic
    The KNode protocol must take into consideration that we need it to be
    extensible, since the limitations of the system are not yet met and more
    importantly, the easier to extend, the easier for anyone to use.

--- src/knode_protocol.rs
+++ src/knode_protocol.rs
@@ -2,21 +2,40 @@ pub struct KNodeMsg {
     sender: u8,
     receiver: u8,
     kind: KNodeMsgKind,
-    payload: KNodePayload,
+    payload: Option<KNodePayload>,
 }
 
 pub enum KNodeMsgKind {
-    Command(u8),
+    Command(KNodeCommand),
+    Response(KNodeResponse),
     Heartbeat,
     Err(KNodeErr),
 }
 
 pub enum KNodePayload {
-    Err(KNodeErr),
-    None,
+    ControlData(f32),
+    Initilize(f32),
+    Info { id: usize, data: [u8, 32], len: usize },
 }
 
 pub enum KNodeErr {
     WrongState,
     Fault,
+    Ok,
+}
+
+pub enum KNodeCommand {
+    Initialize,
+    SetPosition,
+    SetVelocity,
+    SetTorque,
+    GetData,
+}
+
+pub enum KNodeResponse {
+    Initilized,
+    PositionSet,
+    VelocitySet,
+    TorqueSet,
+    DataSent,
 }
