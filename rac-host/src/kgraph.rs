9f505612d99da767a0feb66557f2470dd6ecd88a
resnog <sergio.m.gonzalez.a.94@gmail.com>
2025-10-21 23:43:55 +0200

    fixup! RAC-Host#31: Add `KGraph`

--- rac-host/src/kgraph.rs
+++ rac-host/src/kgraph.rs
@@ -5,3 +5,15 @@ struct KGraph<T> {
     /// KController to use as a reference
     articulations: Vec<Articulation<T>>,
 }
+
+impl<T> KGraph<T> {
+    pub fn new() -> Self {
+        KGraph {
+            articulations: Vec::new(),
+        }
+    }
+
+    pub fn add_node(&mut self, node: Articulation<T>) {
+        self.articulations.push(node);
+    }
+}
